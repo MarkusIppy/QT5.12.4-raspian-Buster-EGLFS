4163b628e0cb1bb3ee7d4fca75a93b476b0b86c8
Markus Ippy <Markus.Ippy1981@gmail.com>
2019-07-09 22:18:59 +0200

    test
    remove ssl and fontconfig

--- compileQT.sh
+++ compileQT.sh
@@ -9,11 +9,13 @@ sudo apt-get -y --force-yes upgrade
 
 echo "Install needed packages"
 sudo apt-get install git
-sudo apt-get install sense-hat libatspi-dev build-essential libfontconfig1-dev libdbus-1-dev libfreetype6-dev libicu-dev libinput-dev libxkbcommon-dev libsqlite3-dev libssl-dev libpng-dev libjpeg-dev libglib2.0-dev libraspberrypi-dev
-sudo apt-get install bluez libbluetooth-dev
-sudo apt-get install libasound2-dev pulseaudio libpulse-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-pulseaudio gstreamer1.0-tools gstreamer1.0-alsa gstreamer-tools
-sudo apt-get install libpq-dev libmariadbclient-dev clang
-sudo apt-get install libegl1-mesa-dev libgbm-dev libgles2-mesa-dev mesa-common-dev
+sudo apt-get install -y sense-hat libatspi-dev build-essential libfontconfig1-dev libdbus-1-dev libfreetype6-dev libicu-dev libinput-dev libxkbcommon-dev libsqlite3-dev libssl-dev libpng-dev libjpeg-dev libglib2.0-dev libraspberrypi-dev
+sudo apt-get install -y bluez libbluetooth-dev
+sudo apt-get install -y libasound2-dev pulseaudio libpulse-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-pulseaudio gstreamer1.0-tools gstreamer1.0-alsa gstreamer-tools
+sudo apt-get install -y libpq-dev libmariadbclient-dev clang
+sudo apt-get install -y libegl1-mesa-dev libgbm-dev libgles2-mesa-dev mesa-common-dev
+sudo apt-get install -y clang
+sudo apt-get install -y libclang-dev
 
 echo "Download QT 5.12.4 Source code"
 wget http://download.qt.io/official_releases/qt/5.12/5.12.4/single/qt-everywhere-src-5.12.4.tar.xz
@@ -35,7 +37,7 @@ sudo chown pi:pi /opt/QT5
 
 echo "Configure QT "
 
-PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig PKG_CONFIG_SYSROOT_DIR=/ \../qt-everywhere-src-5.12.4/configure -v -opengl es2 -eglfs -no-pch -no-gtk -device linux-rasp-pi3-vc4-g++ \-device-option CROSS_COMPILE=/usr/bin/ -opensource -confirm-license -reduce-exports \-force-pkg-config -nomake examples -no-compile-examples -skip qtwayland -skip qtwebengine -release \-qt-pcre -ssl -evdev -system-freetype -fontconfig -glib -gstreamer -prefix /opt/QT5
+PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig PKG_CONFIG_SYSROOT_DIR=/ \../qt-everywhere-src-5.12.4/configure -v -opengl es2 -eglfs -no-pch -no-gtk -no-use-gold-linker -device linux-rasp-pi3-vc4-g++ \-device-option CROSS_COMPILE=/usr/bin/ -opensource -confirm-license -reduce-exports \-force-pkg-config -nomake examples -no-compile-examples -skip qtwayland -skip qtwebengine -release \-qt-pcre -evdev -system-freetype-glib -gstreamer -prefix /opt/QT5
 
 echo "Compile QT with 4 cores "
 make -j4
